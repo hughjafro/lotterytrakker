9e497f8e6e10b7df670de09b4b2aa190573a33ca
Christopher Tolbert <cmtolbert1@gmail.com>
2013-10-18 17:26:43 -0700

    New rails lottery project, mongoDB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lotterytrakker::Application.config.secret_key_base = 'ecbaa2d30b8271fffae8d9d94a4a2c9f52b2f1157acff20e828246841f8d7587b8f90dc8786a23ba249c3a8aa7f0aec55ab1218424cfd96b3cbae715d9dbcde6'
